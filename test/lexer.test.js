193576139631fc888909bc55895e9438164e924c
Ben Gourley <benleighgourley@gmail.com>
2011-11-04 15:25:47 +0000

    Adding parser functionality

--- test/lexer.test.js
+++ test/lexer.test.js
@@ -26,7 +26,7 @@ exports['empty input'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('');
 
-	assert.equal(lexer.lex(), '$end');
+	assert.equal(lexer.lex().type, 'eof');
 
 };
 
@@ -34,26 +34,26 @@ exports['keyword vs. identifier'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('if ift gif ');
 
-	assert.equal(lexer.lex(), 'if');
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'identifier');
+	assert.equal(lexer.lex().type, 'if');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'identifier');
 
 };
 
 exports['simple indentation'] = function (beforeExit, assert) {
 	var lexer = newLexerWithInput('x\n  x\nx\n');
 
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'indent');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'dedent');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), '$end');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'indent');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'dedent');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'eof');
 
 };
 
@@ -61,9 +61,9 @@ exports['consecutive newlines'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('x\n\nx\n');
 
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'identifier');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'identifier');
 
 };
 
@@ -71,12 +71,12 @@ exports['inconsistent indentation'] = function (beforeExit, assert) {
 	
 	var lexer = newLexerWithInput('x\n  x\n\tx');
 
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'indent');
-	assert.equal(lexer.lex(), 'vwhitespace');
-	assert.equal(lexer.lex(), 'identifier');
-	assert.equal(lexer.lex(), 'error');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'indent');
+	assert.equal(lexer.lex().type, 'vwhitespace');
+	assert.equal(lexer.lex().type, 'identifier');
+	assert.equal(lexer.lex().type, 'error');
 
 };
 
@@ -84,7 +84,7 @@ exports['starts with indent'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('  x\n');
 
-	assert.equal(lexer.lex(), 'error');
+	assert.equal(lexer.lex().type, 'error');
 
 };
 
@@ -92,7 +92,7 @@ exports['strings'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('\'Hello, World!\'\n');
 
-	assert.equal(lexer.lex(), 'string');
+	assert.equal(lexer.lex().type, 'string');
 
 };
 
@@ -100,8 +100,8 @@ exports['numbers'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('12345 0.99\n');
 
-	assert.equal(lexer.lex(), 'number');
-	assert.equal(lexer.lex(), 'number');
+	assert.equal(lexer.lex().type, 'number');
+	assert.equal(lexer.lex().type, 'number');
 
 };
 
@@ -109,9 +109,9 @@ exports['literals'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('object: function: array:\n');
 
-	assert.equal(lexer.lex(), 'objectliteral');
-	assert.equal(lexer.lex(), 'functionliteral');
-	assert.equal(lexer.lex(), 'arrayliteral');
+	assert.equal(lexer.lex().type, 'objectliteral');
+	assert.equal(lexer.lex().type, 'functionliteral');
+	assert.equal(lexer.lex().type, 'arrayliteral');
 
 };
 
@@ -125,23 +125,23 @@ exports['literals'] = function (beforeExit, assert) {
 
 };*/
 
-exports['single characters'] = function (beforeExit, assert) {
+/*exports['single characters'] = function (beforeExit, assert) {
 
 	var lexer = newLexerWithInput('[]{}=."~+()¶\n');
 
-	assert.equal(lexer.lex(), 'leftsquarebracket');
-	assert.equal(lexer.lex(), 'rightsquarebracket');
-	assert.equal(lexer.lex(), 'leftbrace');
-	assert.equal(lexer.lex(), 'rightbrace');
-	assert.equal(lexer.lex(), 'equals');
-	assert.equal(lexer.lex(), 'dot');
-	assert.equal(lexer.lex(), 'speechmark');
-	assert.equal(lexer.lex(), 'tilde');
-	assert.equal(lexer.lex(), 'plus');
-	assert.equal(lexer.lex(), 'leftbracket');
-	assert.equal(lexer.lex(), 'rightbracket');
-	assert.equal(lexer.lex(), 'error');
+	assert.equal(lexer.lex().type, 'leftsquarebracket');
+	assert.equal(lexer.lex().type, 'rightsquarebracket');
+	assert.equal(lexer.lex().type, 'leftbrace');
+	assert.equal(lexer.lex().type, 'rightbrace');
+	assert.equal(lexer.lex().type, 'equals');
+	assert.equal(lexer.lex().type, 'dot');
+	assert.equal(lexer.lex().type, 'speechmark');
+	assert.equal(lexer.lex().type, 'tilde');
+	assert.equal(lexer.lex().type, 'plus');
+	assert.equal(lexer.lex().type, 'leftbracket');
+	assert.equal(lexer.lex().type, 'rightbracket');
+	assert.equal(lexer.lex().type, 'error');
 
-};
+};*/
 
 
