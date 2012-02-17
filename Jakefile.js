f94bf790dcbe3330f9545b502a7b7a59e5e81731
Ben Gourley <benleighgourley@gmail.com>
2012-02-17 18:58:33 +0000

    Added docs and docs task in jakefile. Removed runtime.js.

--- Jakefile.js
+++ Jakefile.js
@@ -1,21 +1,49 @@
+/*global task desc */
+
 /*
  * Module dependencies
  */
 var spawn = require('child_process').spawn;
+
+function print(out) {
+  process.stdout.write(out);
+}
 
 /*
  * Run tests: uses the mocha TDD framework
  */
 desc('Run tests');
 task('test', function () {
+
   var test = spawn('./node_modules/mocha/bin/mocha',
       ['--ui', 'bdd', '--colors', '--reporter', 'spec']);
 
-  function print(out) {
-    process.stdout.write(out);
-  }
-
   test.stdout.on('data', print);
   test.stderr.on('data', print);
+
+});
+
+desc('Make documentation');
+task('docs', function () {
+
+  var fs = require('fs');
+
+  var createDocs = function (err, files) {
+    files.forEach(function (file) {
+      if (!/.js$/.test(file)) return;
+      var docs = spawn('docco', [__dirname + '/lib/' + file]);
+      docs.stdout.on('data', print);
+      docs.stderr.on('data', print);
+    });
+  };
+
+  var rm = spawn('rm', ['-rf', __dirname + '/docs']);
+
+  rm.stdout.on('data', print);
+  rm.stderr.on('data', print);
+
+  rm.on('exit', function () {
+    fs.readdir(__dirname + '/lib/', createDocs);
+  });
 
 });
