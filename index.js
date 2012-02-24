b86296e276c658f7ad95cf318a5254536301793b
Ben Gourley <benleighgourley@gmail.com>
2012-02-24 19:30:44 +0000

    Tabs to spaces

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var coverage = process.env.COVERAGE;
 
 module.exports.lexer = coverage
-	? require('./lib-cov/lexer')
-	: require('./lib/lexer');
+  ? require('./lib-cov/lexer')
+  : require('./lib/lexer');
 
 module.exports.parser = coverage
-	? require('./lib-cov/parser')
-	: require('./lib/parser');
+  ? require('./lib-cov/parser')
+  : require('./lib/parser');
 
 module.exports.token = coverage
-	? require('./lib-cov/token')
-	: require('./lib/token');
+  ? require('./lib-cov/token')
+  : require('./lib/token');
