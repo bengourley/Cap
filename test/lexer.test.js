f4e8265920f891ceecc46cad8407aeaa0a299046
Ben Gourley <benleighgourley@gmail.com>
2012-03-20 16:01:46 +0000

    Fixed string parsing in lexer

--- test/lexer.test.js
+++ test/lexer.test.js
@@ -8,15 +8,15 @@
  */
 
 var lexer = require('../').lexer,
-		assert = require('assert');
+    assert = require('assert');
 
 /*
  * Convenience function for creating lexers
  */
 var newLexerWithInput = function (input) {
-	var l = lexer();
-	l.setInput(input);
-	return l;
+  var l = lexer();
+  l.setInput(input);
+  return l;
 };
 
 /*
@@ -25,115 +25,122 @@ var newLexerWithInput = function (input) {
 
 describe('lexer', function () {
 
-	it('should return a whitespace token followed by an ' +
-			'eof token when given empty input', function () {
+  it('should return a whitespace token followed by an ' +
+      'eof token when given empty input', function () {
 
-		var lexer = newLexerWithInput('');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'eof');
+    var lexer = newLexerWithInput('');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'eof');
 
-	});
+  });
 
-	it('should distinguish between a keyword ' +
-			'and an identifier when an identifier starts with ' +
-				'or ends with a keyword', function () {
+  it('should distinguish between a keyword ' +
+      'and an identifier when an identifier starts with ' +
+        'or ends with a keyword', function () {
 
-		var lexer = newLexerWithInput('if ift gif ');
+    var lexer = newLexerWithInput('if ift gif ');
 
-		assert.equal(lexer.lex().type, 'if');
-		assert.equal(lexer.lex().type, 'identifier');
-		assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'if');
+    assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'identifier');
 
-	});
+  });
 
-	it('should handle simple indentation', function () {
-		
-		var lexer = newLexerWithInput('x\n  x\nx\n');
+  it('should handle simple indentation', function () {
+    
+    var lexer = newLexerWithInput('x\n  x\nx\n');
 
-		assert.equal(lexer.lex().type, 'identifier');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'indent');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'identifier');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'dedent');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'identifier');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'eof');
-		
-	});
+    assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'indent');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'dedent');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'eof');
+    
+  });
 
-	it('should consolidate consecutive newlines', function () {
-		
-		var lexer = newLexerWithInput('x\n\nx\n');
+  it('should consolidate consecutive newlines', function () {
+    
+    var lexer = newLexerWithInput('x\n\nx\n');
 
-		assert.equal(lexer.lex().type, 'identifier');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'identifier');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'identifier');
 
-	});
+  });
 
-	it('should return an error token if input ' +
-			'starts with an indent', function () {
+  it('should return an error token if input ' +
+      'starts with an indent', function () {
 
-		var lexer = newLexerWithInput('  x\n');
-		assert.equal(lexer.lex().type, 'error');
+    var lexer = newLexerWithInput('  x\n');
+    assert.equal(lexer.lex().type, 'error');
 
-	});
+  });
 
-	it('should identify a string', function () {
+  it('should identify a string', function () {
 
-		var lexer = newLexerWithInput('\'Hello, World!\'\n');
-		assert.equal(lexer.lex().type, 'string');
+    var lexer = newLexerWithInput('\'Hello, World!\'\n');
+    assert.equal(lexer.lex().type, 'string');
 
-	});
+  });
 
-	it('should identify a number', function () {
+  it('should identify a number', function () {
 
-		var lexer = newLexerWithInput('12345 0.99\n');
+    var lexer = newLexerWithInput('12345 0.99\n');
 
-		assert.equal(lexer.lex().type, 'number');
-		assert.equal(lexer.lex().type, 'number');
+    assert.equal(lexer.lex().type, 'number');
+    assert.equal(lexer.lex().type, 'number');
 
-	});
+  });
 
-	it('should identify a string with escaped single quotes', function () {
+  it('should identify a string with escaped single quotes', function () {
 
-		var lexer = newLexerWithInput('\'That\\\'s nice\'');
-		assert.equal(lexer.lex().type, 'string');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'eof');
+    var lexer = newLexerWithInput('\'That\\\'s nice\'');
+    assert.equal(lexer.lex().type, 'string');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'eof');
 
-		lexer = newLexerWithInput('\'That\\\'s n\\\'ice\'');
-		assert.equal(lexer.lex().type, 'string');
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.equal(lexer.lex().type, 'eof');
+    lexer = newLexerWithInput('\'That\\\'s n\\\'ice\'');
+    assert.equal(lexer.lex().type, 'string');
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.equal(lexer.lex().type, 'eof');
 
-	});
+  });
 
-	it('should discard comments');
+  it('should identify two strings separated by commas', function () {
+    var lexer = newLexerWithInput('\'jim\', \'bob\'');
+    assert.equal(lexer.lex().type, 'string');
+    assert.equal(lexer.lex().type, ',');
+    assert.equal(lexer.lex().type, 'string');
+  });
 
-	it('should consolidate newlines at the start of the file', function () {
-		
-		var lexer = newLexerWithInput('\n\n\nx = 10\n');
+  it('should discard comments');
 
-		assert.equal(lexer.lex().type, 'vwhitespace');
-		assert.notEqual(lexer.lex().type, 'vwhitespace');
+  it('should consolidate newlines at the start of the file', function () {
+    
+    var lexer = newLexerWithInput('\n\n\nx = 10\n');
 
-	});
+    assert.equal(lexer.lex().type, 'vwhitespace');
+    assert.notEqual(lexer.lex().type, 'vwhitespace');
 
-	it('should error on inconsistent indentation', function () {
+  });
 
-		var lexer = newLexerWithInput('hello\n  hi\n	hello');
+  it('should error on inconsistent indentation', function () {
 
-		for (var i = 0; i < 5; i++) {
-			lexer.lex();
-		}
+    var lexer = newLexerWithInput('hello\n  hi\n\thello');
 
-		assert.equal(lexer.lex().type, 'error');
+    for (var i = 0; i < 5; i++) {
+      lexer.lex();
+    }
 
-	});
+    assert.equal(lexer.lex().type, 'error');
+
+  });
 
 });
 
