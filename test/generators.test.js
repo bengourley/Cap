481f4cd26c7a262714067f8e688b522d5560f247
Ben Gourley <bn@grly.me>
2012-03-25 23:08:34 +0100

    Started tests for generators

--- test/generators.test.js
+++ test/generators.test.js
@@ -7,7 +7,7 @@
  * Module dependencies
  */
 
-var lexer = require('../').generators,
+var generators = require('../').generators(),
     assert = require('assert');
 
 /*
@@ -15,5 +15,44 @@ var lexer = require('../').generators,
  */
 
 describe('generators', function () {
+
+  it('should wrap the program in an annonymous fn', function () {
+
+    var output = generators['program']({
+      childNodes : [{ type : 'leaf', value :''}]
+    });
+
+    assert.equal(output, '(function () {\n}());');
+
+  });
+
+  it('should output a leaf node\'s value', function () {
+
+    var output = generators['leaf']({
+      type : 'leaf',
+      value : '10'
+    });
+
+    assert.equal(output, '10');
+
+  });
+
+  it('should determine whether an identifier is in scope', function () {
+
+    var output = generators['identifier']({
+      type : 'identifier',
+      value : 'foo'
+    }, { scope : ['foo'] });
+
+    assert.equal(output, 'foo');
+
+    assert.throws(function () {
+      generators['identifier']({
+        type : 'identifier',
+        value : 'foo'
+      }, { scope : [] });
+    });
+
+  });
 
 });
